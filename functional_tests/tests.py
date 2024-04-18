78a2ae99c6e1faad1fb60c8c9831a75cb9cecf8e
liaoliaoxx <llxx4722021@163.com>
2024-04-18 15:45:42 +0800

    message 12: Use LiveServerTestCase to refactor functional_tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 from selenium.webdriver.common.by import By
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Chrome('D:/develop/chromedriver.exe')
 	
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# 张三听说有一个在线待办事项的应用
 		# 他去看了这个应用的首页
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# 他注意到网页里包含"To-Do"这个词
 		self.assertIn('To-Do', self.browser.title)
@@ -42,5 +42,4 @@ class NewVisitorTest(unittest.TestCase):
 
 		self.fail('Finish the test!')
 
-if __name__ == '__main__':
-	unittest.main()
+
