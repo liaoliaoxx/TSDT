b24d238f18d23608d55ee698509714500fbf9c39
liaoliaoxx <llxx4722021@163.com>
2024-04-18 16:00:58 +0800

    message 13: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
@@ -11,10 +14,18 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element(By.ID,'id_list_table')
-		rows = table.find_elements(By.TAG_NAME,'tr')
-		self.assertIn(row_text,[row.text for row in rows])
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element(By.ID,'id_list_table')
+				rows = table.find_elements(By.TAG_NAME,'tr')
+				self.assertIn(row_text,[row.text for row in rows])
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# 张三听说有一个在线待办事项的应用
@@ -29,16 +40,14 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element(By.ID,'id_new_item')
 		inputbox.send_keys('Buy flowers')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(2)
-		self.check_for_row_in_list_table('1: Buy flowers')
+		self.wait_for_row_in_list_table('1: Buy flowers')
 
 		inputbox = self.browser.find_element(By.ID,'id_new_item')
 		inputbox.send_keys('Give a gift to Lisi')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
-		self.check_for_row_in_list_table('1: Buy flowers')
-		self.check_for_row_in_list_table('2: Give a gift to Lisi')
+		self.wait_for_row_in_list_table('1: Buy flowers')
+		self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
 		self.fail('Finish the test!')
 
