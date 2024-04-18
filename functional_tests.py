b1534a60ac5f0bf9ca3b73348c66885760d72374
liaoliaoxx <llxx4722021@163.com>
2024-04-18 15:26:57 +0800

    message 11: Show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -26,11 +26,11 @@ class NewVisitorTest(unittest.TestCase):
 		header_text = self.browser.find_element(By.TAG_NAME,'h1').text
 		self.assertIn('To-Do',header_text)
 
-		# inputbox = self.browser.find_element(By.ID,'id_new_item')
-		# inputbox.send_keys('Buy flowers')
-		# inputbox.send_keys(Keys.ENTER)
-		# time.sleep(1)
-		# self.check_for_row_in_list_table('1: Buy flowers')
+		inputbox = self.browser.find_element(By.ID,'id_new_item')
+		inputbox.send_keys('Buy flowers')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(2)
+		self.check_for_row_in_list_table('1: Buy flowers')
 
 		inputbox = self.browser.find_element(By.ID,'id_new_item')
 		inputbox.send_keys('Give a gift to Lisi')
